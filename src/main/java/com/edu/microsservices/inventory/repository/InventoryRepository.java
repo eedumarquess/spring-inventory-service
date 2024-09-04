04468049a5fd2f31b511ddbb95c3d455cd8b81a7
Edu <eedumarquess@gmail.com>
2024-09-04 16:37:40 -0300

    FEAT: create base files for inventory service

---
+++ src/main/java/com/edu/microsservices/inventory/repository/InventoryRepository.java
@@ -0,0 +1,8 @@
+package com.edu.microsservices.inventory.repository;
+
+import com.edu.microsservices.inventory.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    boolean existsBySkuCodeAndQuantityIsGreaterThanEqual(String skuCode, Integer quantity);
+}
